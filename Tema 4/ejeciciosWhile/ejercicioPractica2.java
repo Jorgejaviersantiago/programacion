4a010ff763175751a493b7448f739e3e3d3c503d
Jorgejaviersantiago <jsanesc766@g.educaand.es>
2025-10-31 09:23:54 +0100

    ggggtg

--- Tema 4/ejeciciosWhile/ejercicioPractica2.java
+++ Tema 4/ejeciciosWhile/ejercicioPractica2.java
@@ -1,41 +1,27 @@
-
-
 import java.util.Scanner;
 
-public class ejerciciosPractica {
-    
+public class ejercicioPractica2 {
+    //BlueWhile y el while cuando no se con seguridad cuantas veces se tiene que repetir el buble y el ford cuando yo se exactamente cuando se cierra y cuando empieza 
 
     public static void main(String[] args) {
-        
         int num;
         int totalNum = 0;
         int total  = 0;
        
         Scanner escaner=new Scanner(System.in);
          System.out.print("Introduzca número y pulse enter, introduce un número negativo.\n");
-         System.out.print("?");
-         num=escaner.nextInt();
+   
         
-         while(num>=0){
-            total= total + num; // total += totalNum
+         do{
             System.out.print("?");
             num=escaner.nextInt();
-            totalNum++;
-        
-            
-          }
+           if(num>=0){
+                total= total + num; // total += totalNum
+                totalNum++;
+           } 
+          } while(num>0);
        
           System.out.printf("Se han introducido\033[1m  %d \033[0mnúmeros en total y la suma de todos ellos es\033[1m %d \033[0m ",totalNum,total);
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
   
   
     }
---
+++ Tema 4/ejeciciosWhile/ejercicioPractica3.java
@@ -0,0 +1,25 @@
+public class ejercicioPractica3 {
+    
+
+public static void main(String[] args) {
+    
+    int edad;
+    System.out.print("Introduce tu edad:");
+    edad=Integer.parseInt(System.console().readLine());
+
+     for(int i = 1; i <= edad;i++ ){
+        System.out.print(i+" ");
+     }
+
+
+
+
+
+
+
+
+
+
+
+}
+}
