f6063bbc620bc122bc033c7b96d3093c9fc15654
Jorgejaviersantiago <jsanesc766@g.educaand.es>
2025-11-03 11:17:20 +0100

    iugiuybiubpui

--- Tema 4/relacioDeEjercicios/ejercicio9.java
+++ Tema 4/relacioDeEjercicios/ejercicio9.java
@@ -8,59 +8,72 @@ public static void main(String[] args) {
             System.out.println("Este programa te dirá cual es tu horóscopo. ");
             System.out.print("Escribe el número de mes en que nació(1-12): ");
             mes=Integer.parseInt(System.console().readLine());
+           
+    if (mes==12) {
             System.out.print("Ahora introduzca el día : ");
             dia=Integer.parseInt(System.console().readLine());
 
-    if (mes==12 || mes==1) {
-        if(dia>=22 || dia<=19){
+        if(dia>=22 && dia<=31 || dia>=1 && dia<=19){
             System.out.println("Capricornio");
         }
-    }if(mes==1 || mes==2){
-        if(dia>19 || dia<17){
+    }if(mes==1){
+        System.out.print("Ahora introduzca el día : ");
+        dia=Integer.parseInt(System.console().readLine());
+
+        if(dia>=20 && dia<=30 || dia>=1 && dia<=18){
             System.out.println("Acuario");
 
         }
-    }if(mes==2 || mes== 3){
-        if(dia>18 || dia<21){
+    }if(mes==2){
+        System.out.print("Ahora introduzca el día : ");
+        dia=Integer.parseInt(System.console().readLine());
+
+        if(dia>=19 && dia<=28 || dia>=1 && dia<=20){
             System.out.println("Piscis");
     }
-    }if (mes==3 || mes==4) {
-        if(dia>=22 || dia<=19){
+    }if (mes==3) {
+        System.out.print("Ahora introduzca el día : ");
+            dia=Integer.parseInt(System.console().readLine());
+
+        if(dia>=21 && dia<=30 || dia>=1 && dia<=19){
             System.out.println("Aries");
         }
-    }if(mes==4 || mes==5){
-        if(dia>20 || dia<20){
+    }if(mes==4){
+        System.out.print("Ahora introduzca el día : ");
+            dia=Integer.parseInt(System.console().readLine());
+
+        if(){
             System.out.println("Tauro");
 
         }
-    }if(mes==5|| mes== 6){
-        if(dia>22 || dia<21){
+    }if(mes==5){
+        if(){
             System.out.println("Gémini");
     }
-    }if(mes==6 || mes==7){
-        if(dia>20 || dia<22){
+    }if(mes==6){
+        if(){
             System.out.println("Cancer");
 
         }
-    }if(mes==7 || mes== 8){
-        if(dia>18 || dia<21){
+    }if(mes==7){
+        if(){
             System.out.println("Leo");
     }
-    }if (mes==8 || mes==9) {
-        if(dia>=22 || dia<=19){
+    }if (mes==8) {
+        if(){
             System.out.println("Virgo");
         }
-    }if(mes==9 || mes==10){
-        if(dia>20 || dia<20){
+    }if(mes==9){
+        if(){
             System.out.println("Libra");
 
         }
-    }if(mes==10 || mes== 11){
-        if(dia>22 || dia<21){
+    }if(mes==10){
+        if(){
             System.out.println("Escorpio");
     }
-    }if(mes==11 || mes== 12){
-        if(dia>22 || dia<21){
+    }if(mes==11){
+        if(){
             System.out.println("Sagitario");
     }
     }
---
+++ tema6/relacioDeEjercicios6/ejercicio11.java
@@ -0,0 +1,21 @@
+public class ejercicio11 {
+
+    
+
+    public static void main(String[] args) {
+      int numero;
+    
+      System.out.printf("\033[32m");
+    for( ; ; ) {
+        numero=(char) (Math.random()*95)+32;
+        System.out.printf("%c",numero);
+    }
+    
+    
+    
+    
+    
+    
+    
+    }
+}
