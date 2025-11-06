a4d1e958c94227b1e87ad772dc3a1833739dda0d
Jorgejaviersantiago <jsanesc766@g.educaand.es>
2025-11-06 08:52:28 +0100

    rettrtet

--- Tema 4/relacioDeEjercicios/ejercicio9.java
+++ Tema 4/relacioDeEjercicios/ejercicio9.java
@@ -8,78 +8,110 @@ public static void main(String[] args) {
             System.out.println("Este programa te dirá cual es tu horóscopo. ");
             System.out.print("Escribe el número de mes en que nació(1-12): ");
             mes=Integer.parseInt(System.console().readLine());
-           
-    if (mes==12) {
-            System.out.print("Ahora introduzca el día : ");
+            System.out.print("Ahora introducca el día en que nacio: ");
             dia=Integer.parseInt(System.console().readLine());
 
-        if(dia>=22 || dia<=31 ){
-            System.out.println("Capricornio");
-        }else if(dia<=19) {
-            System.out.println("Capricornio");
 
-        }else
-    }if(mes==1){
-        System.out.print("Ahora introduzca el día : ");
-        dia=Integer.parseInt(System.console().readLine());
+                        switch (mes) {
+                       case 12:
+                             if (dia<21) {
+                                System.out.println("Sagitario");
+                             }else{
+                                System.out.println("Capricornio");
+                             }
 
-        if(dia>=20 && dia<=30 || dia>=1 && dia<=18){
-            System.out.println("Acuario");
+                      break;
+                      case 1:
+                         if(dia<21){
+                            System.out.println("Capricornio");
 
-        }
-    }if(mes==2){
-        System.out.print("Ahora introduzca el día : ");
-        dia=Integer.parseInt(System.console().readLine());
+                         }else{
+                            System.out.println("Acuario");
 
-        if(dia>=19 && dia<=28 || dia>=1 && dia<=20){
-            System.out.println("Piscis");
-    }
-    }if (mes==3) {
-        System.out.print("Ahora introduzca el día : ");
-            dia=Integer.parseInt(System.console().readLine());
+                         }
+                     break;
+                     case 2:
+                         if(dia<20){
+                            System.out.println("Acuario");
 
-        if(dia>=21 && dia<=30 || dia>=1 && dia<=19){
-            System.out.println("Aries");
-        }
-    }if(mes==4){
-        System.out.print("Ahora introduzca el día : ");
-            dia=Integer.parseInt(System.console().readLine());
+                         }else{
+                            System.out.println("Piscis");
 
-        if(){
-            System.out.println("Tauro");
+                         }
+                     break;
+                     case 3:
+                         if(dia<21){
+                            System.out.println("Piscis");
 
-        }
-    }if(mes==5){
-        if(){
-            System.out.println("Gémini");
-    }
-    }if(mes==6){
-        if(){
-            System.out.println("Cancer");
+                         } else{
+                            System.out.println("Aries");
+                         }
+                      break;
+                      case 4:
+                         if (dia<21) {
+                            System.out.println("Aries");
+                         }else{
+                            System.out.println("Tauros");
+                         }
+                      break;
+                      case 5:
+                         if (dia<20) {
+                            System.out.println("Tauros");
+                         }else
+                         {
+                            System.out.println("Géminis");
+                         }
+                      break;
+                      case 6:
+                         if (dia<22) {
+                            System.out.println("Géminis");
 
-        }
-    }if(mes==7){
-        if(){
-            System.out.println("Leo");
-    }
-    }if (mes==8) {
-        if(){
-            System.out.println("Virgo");
-        }
-    }if(mes==9){
-        if(){
-            System.out.println("Libra");
+                         }else{
+                            System.out.println("cancer");
+                         }
+                      break;
+                      case 7:
+                         if (dia<22) {
+                            System.out.println("cancer");
+                         }else{
+                            System.out.println("Leo");
+                         }
+                     break;
+                     case 8:
+                         if (dia<24) {
+                                System.out.println("Leo");
+                         }else{
+                             System.out.println("Virgo");
 
-        }
-    }if(mes==10){
-        if(){
-            System.out.println("Escorpio");
-    }
-    }if(mes==11){
-        if(){
-            System.out.println("Sagitario");
-    }
-    }
+                         }
+                     break;
+                    case 9: 
+                         if (dia<23) {
+                            System.out.println("Virgo");
+                         }else{
+                            System.out.println("Libra");
+
+                         }
+                    break; 
+                    case 10:
+                         if (dia<23) {
+                            System.out.println("Libra");
+                         }
+                         else{
+                            System.out.println("Escorpio");
+                         }
+                    break;
+                    case 11:
+                         if (dia<23) {
+                            System.out.println("Escorpio");
+                         }else{
+                            System.out.println("Sagitario");
+                         }
+                    break;
+                    default:
+                        break;
+                }
+
 
 
 
