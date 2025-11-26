87d64a277e5d6e4be84d6b30a188fdbec6f8f3ba
Jorgejaviersantiago <jsanesc766@g.educaand.es>
2025-11-26 13:53:08 +0100

    fyugfy

--- Tema 4/ejeciciosWhile/ejercicioPractica.java
+++ Tema 4/ejeciciosWhile/ejercicioPractica.java
@@ -1,5 +1,11 @@
 public class ejercicioPractica {
     public static void main(String[] args) {
         int numMaz;
+        int numMin;
+        int longitud;
+        int temporal;
+        
+        
+    
     }
 }
