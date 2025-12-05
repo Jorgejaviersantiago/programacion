8f5fcda84fe9e59f74a9654daa997fecfbef2b4f
Jorgejaviersantiago <jsanesc766@g.educaand.es>
2025-12-05 10:26:25 +0100

    fyiewfwiewq

---
+++ Tema 4/ejeciciosWhile/ejercicio47.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class ejercicio47 {
+    
+
+    public static void main(String[] args) {
+        
+        int altura;
+        int distancia;
+
+        Scanner escaner = new Scanner(System.in);
+        
+        System.out.print("Introducce la altura(5 como minimo): ");
+        altura=escaner.nextInt();
+       
+        System.out.print("Introducce el espacio entre numeros(1 como minimo): ");
+        distancia=escaner.nextInt();
+
+        if ((altura <5)||(distancia <1)){
+            System.out.println("");
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void dibujarGarfica(int altura, int distanci) {
+        
+        System.out.print("");
+
+
+
+
+    }
+
+
+
+}
